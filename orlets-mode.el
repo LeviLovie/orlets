3a35d58aa5f06cef2fd5c215623145bb6805b52e
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2023-11-27 00:06:22 +0700

    Add orlets-mode.el for emacs syntax

---
+++ orlets-mode.el
@@ -0,0 +1,33 @@
+(defconst orlets-mode-syntax-table
+  (with-syntax-table (copy-syntax-table)
+    ;; C/C++ style comments
+    (modify-syntax-entry ?/ ". 124b")
+    (modify-syntax-entry ?* ". 23")
+    (modify-syntax-entry ?\n "> b")
+    ;; Chars are the same as strings
+    (modify-syntax-entry ?' "\"")
+    (syntax-table))
+  "Syntax table for `orlets-mode'.")
+
+(eval-and-compile
+  (defconst orlets-keywords
+    '("include")))
+
+(defconst orlets-highlights
+  `((,(regexp-opt orlets-keywords 'symbols) . font-lock-keyword-face)
+    (,(rx "(" (group-n 1 (zero-or-more (not (any "()")))) ")")
+     (1 font-lock-function-name-face))))
+
+;;;###autoload
+(define-derived-mode orlets-mode prog-mode "orlets"
+  "Major Mode for editing Orlets source code."
+  :syntax-table orlets-mode-syntax-table
+  (setq font-lock-defaults '(orlets-highlights))
+  (setq-local comment-start "// "))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.orlets\\'" . orlets-mode))
+
+(provide 'orlets-mode)
+
+;;; orlets-mode.el ends here
